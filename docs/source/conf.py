44e601ea38a671709017c380290fe237b2a8c0a2
J.G.Aguado <jon-garcia@hotmail.com>
2023-08-14 20:00:58 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx_copybutton',
 ]
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
