d9095fce6f13ba1c590a5e79b0483c012e1a2456
J.G.Aguado <jon-garcia@hotmail.com>
2023-10-09 21:01:21 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,26 @@
-from datetime import date
+# ****************************************************************
 
 # Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+from datetime import date
+
+
+# -- Project information -----------------------------------------------------
 
 project = 'ElectroLab'
-
 author = 'J.G.Aguado'
 email = 'jon-garcia@hotmail.com'
 
@@ -15,8 +30,6 @@ version = '3'
 today = date.today()
 compile_date = today.strftime("%B %d, %Y")
 
-# -- Project information
-
 rst_epilog  = """
 .. |Product| replace:: %s
 .. |Author| replace:: %s
@@ -25,27 +38,38 @@ rst_epilog  = """
 .. |Date| replace:: %s
 """ % (project, author, email, version, compile_date)
 
-
 # -- General configuration
-
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
+
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-# -- Options for HTML output
+# -- Options for EPUB output
+epub_show_urls = "footnote"
 
-html_theme = 'sphinx_rtd_theme'
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     'logo_only': True,
     'navigation_depth': 5,
@@ -55,11 +79,8 @@ html_theme_options = {
 html_context = {}
 
 html_logo = "images/logo/White.png"
-html_show_sourcelink = True
-html_static_path = ['_static']
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
-
-#def setup(app):
-#    app.add_css_file('my_theme.css')
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
